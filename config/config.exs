2394e951185c0769466e9602a4214bb87264b8bd
vaibhav-dhoke <vaibhav-dhoke@vaibhavdhoke-ThinkPad-E560>
2017-09-22 12:15:07 +0530

    First Commit

---
+++ config/config.exs
@@ -0,0 +1,27 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :phoenix_chat,
+  ecto_repos: [PhoenixChat.Repo]
+
+# Configures the endpoint
+config :phoenix_chat, PhoenixChat.Endpoint,
+  url: [host: "10.10.60.110"],
+  secret_key_base: "XSFXDqq3zosZl3Fkx9EC6FeK4MQsXkQ/vMalgR0UKQ0uLePU5qe95nWrRkMUXvO/",
+  render_errors: [view: PhoenixChat.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: PhoenixChat.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
